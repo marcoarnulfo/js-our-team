baf9fb8309e5f18c87b2c79b7ee97f1be040d604
Marco Arnulfo <arnulfomarco7@gmail.com>
2022-10-20 16:24:57 +0200

    end project with bonus

--- bonus/assets/js/main.js
+++ bonus/assets/js/main.js
@@ -1,57 +1,39 @@
-console.log("hello");
-/*
-Utilizzando i dati forniti, creare un array di 
-oggetti per rappresentare i membri del team.
-Ogni membro è caratterizzato dalle seguenti 
-informazioni: nome, ruolo e foto.
-*/
-
-/* MILESTONE 0
-
-- Creare un array di oggetti con le informaizoni fornite.
-
-*/
 
 const team = [
     { 
         Name: 'Wayne Barnett',
         Role: 'Founder & CEO',
-        Image: 'wayne-barnett-founder-ceo.jpg'
+        Image: "./assets/img/wayne-barnett-founder-ceo.jpg"
     },
     {
         Name: 'Angela Caroll',
         Role: 'Chief Editor',
-        Image: 'angela-caroll-chief-editor.jpg'
+        Image: "./assets/img/angela-caroll-chief-editor.jpg" 
     },
     {
         Name: 'Waleter gordon',
         Role: 'Chief Editor',
-        Image: 'angela-caroll-chief-editor.jpg'
+        Image: "./assets/img/walter-gordon-office-manager.jpg"
     },
     {
         Name: 'Angela Lopez',
         Role: 'Social Media Manager',
-        Image: 'angela-lopez-chief-editor.jpg'
+        Image: "./assets/img/angela-lopez-social-media-manager.jpg"
     },
     {
         Name: 'Scott Estrada',
         Role: 'Developer',
-        Image: 'scott-estrada-chief-editor.jpg'
+        Image: "./assets/img/scott-estrada-developer.jpg"
     },
     {
         Name: 'Barbara Ramos',
         Role: 'Grafic designer',
-        Image: 'barbara-ramos-chief-editor.jpg'
+        Image: "./assets/img/barbara-ramos-graphic-designer.jpg"
     }
 ]
 
-
 console.log(team);
-/* MILESTONE 1
 
-- Stampare su console le informazioni di nome, ruolo e la stringa della foto
-
-*/
 
 /*
 for (let index = 0; index < team.length; index++) {
@@ -61,25 +43,45 @@ for (let index = 0; index < team.length; index++) {
 */
 const h2Element = document.querySelector("h2")
 const ulElement = document.querySelector("ul")
+const immagine = document.querySelector("img")
+
+const row = document.querySelector(".row")
 
 for (const key in team) {
     const secondEl = team[key];
     //console.log(secondEl.Name);
     //console.log(secondEl.Role);
     //console.log(secondEl.Image);
-    const liElement = document.createElement("li")
-    const secondLi = document.createElement("li")
-    const terzoLi = document.createElement("li")
-    console.log(liElement);
-    ulElement.append(liElement)
-    liElement.append(secondEl.Name)
-    ulElement.append(secondLi)
-    secondLi.append(secondEl.Role)
-    ulElement.append(terzoLi)
-    terzoLi.append(secondEl.Image)
+    const col = document.createElement("div")
+    col.classList.add("col", "mt-5")
+    const nameElement = document.createElement("h4")
+    nameElement.classList.add("pt-2")
+    const roleElement = document.createElement("div")
+    roleElement.classList.add("fst-italic")
+    const imageElement = document.createElement("img")
+    row.append(col)
+    col.append(imageElement, nameElement, roleElement)
+    imageElement.src = secondEl.Image
+    nameElement.append(secondEl.Name)
+    roleElement.append(secondEl.Role)
+
+    //const liElement = document.createElement("li") // nome
+    //const secondLi = document.createElement("li") // ruolo
+    //const terzoLi = document.createElement("li") // immagine
+    
+    
+    //ulElement.append(liElement, secondLi, terzoLi)
+    //liElement.append(secondEl.Name)
+    //secondLi.append(secondEl.Role)
+    //terzoLi.append(secondEl.Image)
+    //terzoLi.append(test)
+
 }
-/* MILESTONE 1
-
-- Stampare le stesse informazioni su DOM sottoforma di stringhe
-
+/*
+const immagine = document.querySelector("img");
+immagine.src = team[3]
+*/
+/*
+    BONUS 1: Trasformare la stringa foto in una immagine effettiva
+    BONUS 2: Organizzare i singoli membri in card/schede
 */
