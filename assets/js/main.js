58f014066379aaac3363573bfd2da268e968bdc2
Marco Arnulfo <arnulfomarco7@gmail.com>
2022-10-20 14:14:00 +0200

    initial commict. add index, css, js and bootstrap

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+console.log("hello");
+/*
+Utilizzando i dati forniti, creare un array di 
+oggetti per rappresentare i membri del team.
+Ogni membro è caratterizzato dalle seguenti 
+informazioni: nome, ruolo e foto.
+*/
+
+/* MILESTONE 0
+
+- Creare un array di oggetti con le informaizoni fornite.
+
+*/
+
+/* MILESTONE 1
+
+- Stampare su console le informazioni di nome, ruolo e la stringa della foto
+
+*/
+
+/* MILESTONE 1
+
+- Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+*/
