135f9e099ef54099331048c6cd6552a65ba40c2c
Marco Arnulfo <arnulfomarco7@gmail.com>
2022-10-20 16:26:24 +0200

    fix comments

--- bonus/assets/js/main.js
+++ bonus/assets/js/main.js
@@ -34,24 +34,11 @@ const team = [
 
 console.log(team);
 
-
-/*
-for (let index = 0; index < team.length; index++) {
-    const element = team[index];
-    //console.log(element);
-}
-*/
-const h2Element = document.querySelector("h2")
-const ulElement = document.querySelector("ul")
 const immagine = document.querySelector("img")
-
 const row = document.querySelector(".row")
 
 for (const key in team) {
     const secondEl = team[key];
-    //console.log(secondEl.Name);
-    //console.log(secondEl.Role);
-    //console.log(secondEl.Image);
     const col = document.createElement("div")
     col.classList.add("col", "mt-5")
     const nameElement = document.createElement("h4")
@@ -64,24 +51,4 @@ for (const key in team) {
     imageElement.src = secondEl.Image
     nameElement.append(secondEl.Name)
     roleElement.append(secondEl.Role)
-
-    //const liElement = document.createElement("li") // nome
-    //const secondLi = document.createElement("li") // ruolo
-    //const terzoLi = document.createElement("li") // immagine
-    
-    
-    //ulElement.append(liElement, secondLi, terzoLi)
-    //liElement.append(secondEl.Name)
-    //secondLi.append(secondEl.Role)
-    //terzoLi.append(secondEl.Image)
-    //terzoLi.append(test)
-
 }
-/*
-const immagine = document.querySelector("img");
-immagine.src = team[3]
-*/
-/*
-    BONUS 1: Trasformare la stringa foto in una immagine effettiva
-    BONUS 2: Organizzare i singoli membri in card/schede
-*/
